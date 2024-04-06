890b8a1dda763a975120ee8c0cb8b72c96333e7e
kevin-ar_e <ayak57085@gmail.com>
2024-04-06 17:05:15 +0100

    add_config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  config.hosts << "animeapi-y5somzm0.b4a.run"
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
