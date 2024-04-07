6e1de7bd1e04b45e3058bdc52f050e0434d92b2b
kevin-ar_e <ayak57085@gmail.com>
2024-04-07 11:59:34 +0100

    new_update1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,6 @@ Rails.application.configure do
 
   config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 
-  config.hosts << "animeapi-y5somzm0.b4a.run"
-  
+  config.hosts << "animeapi2-pxhv5mqq.b4a.run"
+
 end
